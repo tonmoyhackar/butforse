6001496982d26309f897f9b5913fbbde6449cc9d
tonmoyhackar <108315326+tonmoyhackar@users.noreply.github.com>
2022-08-27 23:38:57 +0600

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,37 @@
+import os
+os.system("clear")
+G="\033[1;32m"
+B="\033[1;34m"
+Y="\033[1;33m"
+P="\033[1;35m"
+C="\033[1;36m"
+
+design="""
+{G} ______   ___   ____   ___ ___   ___   __ __ 
+{G}|      | /   \ |    \ |   |   | /   \ |  |  |
+{G}|      ||     ||  _  || _   _ ||     ||  |  |   
+{G}|_|  |_||  O  ||  |  ||  \_/  ||  O  ||  ~  |
+{G}  |  |  |     ||  |  ||   |   ||     ||___, |
+{G}  |  |  |     ||  |  ||   |   ||     ||     |  
+{G}  |__|   \___/ |__|__||___|___| \___/ |____/ 
+""".format(P=P,C=C,G=G,Y=Y) 
+
+
+logo=("""{B}
+**************************************************
+{G}Remember >>  •••Allah Always Watching You•••
+{B}**************************************************
+{C} ____We Work To Sarve You a Clean Cyber Space____
+{Y}   ____Well Come To Tonmoy-Hacker's World____
+{B}**************************************************
+{ascii}
+{B}**************************************************
+{G}Author   >>  TONMOY-HASAN [TONMOY-YT]
+{Y}Facebook >>  https://facebook.com/tonmoyhasan88
+{C}Github   >>  https://github.com/tonmoyhacker 
+{B}**************************************************
+""".format(ascii=design,B=B,C=C,G=G,Y=Y))
+
+User="TONMOY-YT"
+Pwd="TONMOY-TOXIC"
+Permission="On"
